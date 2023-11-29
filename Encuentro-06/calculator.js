2409c77c7157982633afa844c3ee981c8bcbd127
AlquimistaMDev <148398882+alquimistamdev@users.noreply.github.com>
2023-11-29 19:04:46 -0300

    Fix issues

--- Encuentro-06/calculator.js
+++ Encuentro-06/calculator.js
@@ -1,8 +1,12 @@
+const operators = ['/', '*', '-', '+']
 let displayValue = ''
 
-function appendToDisplay (value) {
-  displayValue += value
-  document.getElementById('display').value = displayValue
+function appendToDisplay (value) { 
+  const lastcharacter = displayValue.charAt(displayValue.length - 1);
+  if (!operators.includes(lastcharacter) || !operators.includes(value)) {
+    displayValue += value
+    document.getElementById('display').value = displayValue
+  }
 }
 
 function clearDisplay () {
